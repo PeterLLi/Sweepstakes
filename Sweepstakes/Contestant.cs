50502a0860432990258a5bdfdb4f26e4927d49f5
rmghost <youngenli@gmail.com>
2018-07-12 07:55:25 -0500

    initial files and basic menu added

---
+++ Sweepstakes/Contestant.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Sweepstakes
+{
+    public class Contestant
+    {
+        public Contestant()
+        {
+        }
+    }
+}
---
+++ Sweepstakes/SweepstakesManager.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Sweepstakes
+{
+    public class SweepstakesManager
+    {
+        public SweepstakesManager()
+        {
+        }
+    }
+}
